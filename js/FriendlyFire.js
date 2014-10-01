42c08904303fbbe8f2f932c19647b6a2cb957329
adf5051 <adf5051@rit.edu>
2014-10-01 17:53:02 -0400

    no message

---
+++ js/FriendlyFire.js
@@ -0,0 +1,38 @@
+/*
+	Friendly Fire
+	
+	Alex Fuerst, 
+	Mario Chuman,
+	David Eberdling,
+	Brian Nugent,
+	Ryan Farrel,
+
+	Game Design and Development 2
+	10/2/2014
+
+*/
+
+//Friendly Fire.js - this is the control or main game manager
+
+"use strict";
+
+//reate the global application object if it doesn't exist
+var app = app || {};
+
+//create helper constant 'keycodes' for event handling
+app.KEYBOARD = 
+{
+	"KEY_LEFT": 37,
+	"KEY_RIGHT": 38,
+	"KEY_SPACE": 32
+};
+
+//create array of images to pass into objects
+app.IMAGES = 
+{
+	teamImage: "images/shark.png"
+};
+
+//create array of keypresses
+app.keydown[];
+
